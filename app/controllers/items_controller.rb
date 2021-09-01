a095830a3947fe39019eb90e8a85a3517cd55e73
James Yeates <jimibue@gmail.com>
2021-09-01 10:25:50 -0600

    get data

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
 
   def app
-    render component: 'App'
+    render component: 'App' 
+  end
+
+  def index
+     items = Item.all.order(likes: :desc)
+     #  render Component 'Items', props:{items:items}
+   render json: items
   end
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,21 @@
+import axios from "axios";
 import React, { useState } from "react";
+import Items from "./Items";
 
 const App = () => {
+  const getItems = async () => {
+    try {
+      // does this chucnk of code and if fails goes to catch block
+      let res = await axios.get("/items");
+      console.log(res);
+      console.log(res.data);
+    } catch (err) {}
+  };
   return (
     <div>
       <h1>App</h1>
+      <button onClick={getItems}>get Items</button>
+      <Items />
     </div>
   );
 };
