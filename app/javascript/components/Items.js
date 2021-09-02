7c0d0d2e2fd7243d65ff909467773f127bacacb0
James Yeates <jimibue@gmail.com>
2021-09-02 08:46:35 -0600

    bug fix

--- app/javascript/components/Items.js
+++ app/javascript/components/Items.js
@@ -5,8 +5,7 @@ const Items = ({ items, deleteItem }) => {
   return (
     <div style={{ margin: "10px", border: "3px solid red" }}>
       <h1>Items</h1>
-      <Item deleteItem={deleteItem} key={item.id} {...item} />
-      <Item deleteItem={deleteItem} key={item.id} {...item} />
+
       {items.map((item) => (
         <Item deleteItem={deleteItem} key={item.id} {...item} />
         // <Item key={item.id} id={item.id} likes={item.likes}/>
